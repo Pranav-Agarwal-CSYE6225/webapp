21e1039817a6b3f78ec32678801455784bb15f3f
Pranav-Agarwal <agarwal.pran@northeastern.edu>
2021-10-04 23:07:31 -0400

    Added email validation and password encryption
    Uses the email-verification npm package for verifying emails and the bcrypt npm package for encrpyting passwords for storage in the database

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,22 +1,31 @@
 'use strict';
 const User = require('../models/user.model');
+const validator = require("email-validator");
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
 exports.create = function(req, res) {
   let {first_name,last_name,username,password} = req.body;
   if(first_name==null || last_name==null || username==null || password==null){
     res.status(400).send({ error:true, message: 'Missing required fields' });
   }
+  else if(!validator.validate(username)){
+    res.status(400).send({ error:true, message: 'Invalid email' });
+  }
   else{
     User.findUser(username, function(err, user) {
       if (err) res.status(400).send({ error:true, message: 'Bad request' });
       else if (user[0]!=null) res.status(400).send({ error:true, message: 'username already exists' });
       else{
-        User.create(first_name,last_name,username,password, function(err, user) {
+        bcrypt.hash(password, saltRounds, function(err, hash) {
           if (err) res.status(400).send({ error:true, message: 'Bad request' });
-          User.findUser(username, function(err, user) {
-              if (err) res.status(400).send({ error:true, message: 'Bad request' });
-              delete user[0].password;
-              res.status(201).json(user[0]);
+          User.create(first_name,last_name,username,hash, function(err, user) {
+            if (err) res.status(400).send({ error:true, message: 'Bad request' });
+            User.findUser(username, function(err, user) {
+                if (err) res.status(400).send({ error:true, message: 'Bad request' });
+                delete user[0].password;
+                res.status(201).json(user[0]);
+            });
           });
         });
       }
@@ -32,11 +41,16 @@ exports.getUser = function(req, res) {
   User.findUser(username, function(err, user) {
     if (err) res.status(400).send({ error:true, message: 'Bad request' });
     else if (user[0]==null) res.status(400).send({ error:true, message: 'User not found' });
-    else if(user[0].password!=password) res.status(403).send({ error:true, message: 'Wrong Password' });
-    else{
-      delete user[0].password;
-      res.status(200).json(user[0]);
-    } 
+    else {
+      bcrypt.compare(password, user[0].password, function(err, result) {
+        if (err) res.status(400).send({ error:true, message: 'Bad request' });
+        if(!result) res.status(403).send({ error:true, message: 'Wrong Password' });
+        else{
+          delete user[0].password;
+          res.status(200).json(user[0]);
+        } 
+      });
+    }
   });
 };
 
@@ -48,17 +62,25 @@ exports.update = function(req, res) {
   User.findUser(username,function(err, userToUpdate) {
     if (err) res.status(400).send({ error:true, message: 'Bad request' });
     else if (userToUpdate[0]==null) res.status(400).send({ error:true, message: 'User not found' });
-    else if(userToUpdate[0].password!=password) res.status(403).send({ error:true, message: 'Wrong Password' });      
-    else{
-      console.log(req.body);
-      let {first_name,last_name,username,password} = req.body;
-      if(first_name==null) first_name = userToUpdate[0].first_name;
-      if(last_name==null) last_name = userToUpdate[0].last_name;
-      if(password==null) password = userToUpdate[0].password;
-      console.log(first_name+" "+last_name+" "+userToUpdate[0].username+" "+password)
-      User.update(first_name,last_name,userToUpdate[0].username,password, function(err, user) {
+    else {
+      bcrypt.compare(password, userToUpdate[0].password, function(err, result) {
         if (err) res.status(400).send({ error:true, message: 'Bad request' });
-        res.status(204).send({ error:false, message: 'User successfully updated' });
+        if(!result) res.status(403).send({ error:true, message: 'Wrong Password' });  
+        else{
+          console.log(req.body);
+          let {first_name,last_name,username,password} = req.body;
+          if(first_name==null) first_name = userToUpdate[0].first_name;
+          if(last_name==null) last_name = userToUpdate[0].last_name;
+          if(password==null) password = userToUpdate[0].password;
+          console.log(first_name+" "+last_name+" "+userToUpdate[0].username+" "+password)
+          bcrypt.hash(password, saltRounds, function(err, hash) {
+            if (err) res.status(400).send({ error:true, message: 'Bad request' });
+            User.update(first_name,last_name,userToUpdate[0].username,hash, function(err, user) {
+              if (err) res.status(400).send({ error:true, message: 'Bad request' });
+              res.status(204).send({ error:false, message: 'User successfully updated' });
+            });
+          });
+        }
       });
     }
   });
