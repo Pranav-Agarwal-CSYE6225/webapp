1d074998a46e321a55842f63d007d9a36b9c5303
Pranav-Agarwal <agarwal.pran@northeastern.edu>
2021-10-17 16:48:43 -0400

    Replaced callbacks with promises
    Refactored code to use promises with async and await instead of callbacks to improve code readibility and maintainibility

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,8 +4,8 @@ const validator = require("email-validator");
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
-exports.create = function(req, res) {
-  let {first_name,last_name,username,password} = req.body;
+exports.create = async function(req, res) {
+  const {first_name,last_name,username,password} = req.body;
   if(first_name==null || last_name==null || username==null || password==null){
     res.status(400).send({ error:true, message: 'Missing required fields' });
   }
@@ -13,72 +13,81 @@ exports.create = function(req, res) {
     res.status(400).send({ error:true, message: 'Invalid email' });
   }
   else{
-    User.findUser(username, function(err, user) {
-      if (err) res.status(400).send({ error:true, message: 'Bad request' });
-      else if (user[0]!=null) res.status(400).send({ error:true, message: 'username already exists' });
-      else{
-        bcrypt.hash(password, saltRounds, function(err, hash) {
-          if (err) res.status(400).send({ error:true, message: 'Bad request' });
-          User.create(first_name,last_name,username,hash, function(err, user) {
-            if (err) res.status(400).send({ error:true, message: 'Bad request' });
-            User.findUser(username, function(err, user) {
-                if (err) res.status(400).send({ error:true, message: 'Bad request' });
-                delete user[0].password;
-                res.status(201).json(user[0]);
-            });
-          });
-        });
+    try{
+      const oldUser = await User.findUser(username);
+      if(oldUser[0]!=null){
+        res.status(400).send({ error:true, message: 'username already exists' });
+        return;
       }
-    });
+      const hash = await bcrypt.hash(password, saltRounds);
+      await User.create(first_name,last_name,username,hash);
+      const newUser = await User.findUser(username);
+      delete newUser[0].password;
+      res.status(201).json(newUser[0]);
+      return;
+    }
+    catch(err){
+      console.log(err);
+      res.status(400).send({ error:true, message: 'bad request' });
+    }
   }
 };
 
-exports.getUser = function(req, res) {
+exports.getUser = async function(req, res) {
   const token = req.headers.authorization.split(" ")[1]
   const [username,password] = Buffer.from(token,'base64').toString('ascii').split(':');
-  User.findUser(username, function(err, user) {
-    if (err) res.status(400).send({ error:true, message: 'Bad request' });
-    else if (user[0]==null) res.status(400).send({ error:true, message: 'User not found' });
-    else {
-      bcrypt.compare(password, user[0].password, function(err, result) {
-        if (err) res.status(400).send({ error:true, message: 'Bad request' });
-        if(!result) res.status(403).send({ error:true, message: 'Wrong Password' });
-        else{
-          delete user[0].password;
-          res.status(200).json(user[0]);
-        } 
-      });
+  try{
+    const user = await User.findUser(username);
+    if(user[0]==null){
+      res.status(400).send({ error:true, message: 'User not found' });
+      return;
     }
-  });
+    else if(!(await bcrypt.compare(password, user[0].password))){
+      res.status(400).send({ error:true, message: 'Wrong Password' });
+      return;
+    }
+    else{
+      delete user[0].password;
+      res.status(200).json(user[0]);
+      return;
+    }
+  }
+  catch(err){
+    console.log(err);
+    res.status(400).send({ error:true, message: 'bad request' });
+  }
 };
 
-exports.update = function(req, res) {
+exports.update = async function(req, res) {
   const token = req.headers.authorization.split(" ")[1]
   const [username,password] = Buffer.from(token,'base64').toString('ascii').split(':');
-  User.findUser(username,function(err, userToUpdate) {
-    if (err) res.status(400).send({ error:true, message: 'Bad request' });
-    else if (userToUpdate[0]==null) res.status(400).send({ error:true, message: 'User not found' });
-    else {
-      bcrypt.compare(password, userToUpdate[0].password, function(err, result) {
-        if (err) res.status(400).send({ error:true, message: 'Bad request' });
-        if(!result) res.status(403).send({ error:true, message: 'Wrong Password' });  
-        else{
-          let {first_name,last_name,username,password,account_created,account_updated} = req.body;
-          if(first_name==null) first_name = userToUpdate[0].first_name;
-          if(last_name==null) last_name = userToUpdate[0].last_name;
-          if(password==null) password = userToUpdate[0].password;
-          if(username!=null || account_created!=null || account_updated!=null) res.status(400).send({ error:true, message: 'Bad request' });
-          else{
-            bcrypt.hash(password, saltRounds, function(err, hash) {
-              if (err) res.status(400).send({ error:true, message: 'Bad request' });
-              User.update(first_name,last_name,userToUpdate[0].username,hash, function(err, user) {
-                if (err) res.status(400).send({ error:true, message: 'Bad request' });
-                res.status(204).send({ error:false, message: 'User successfully updated' });
-              });
-            });
-          }
-        }
-      });
+  try{
+    const userToUpdate = await User.findUser(username);
+    if(userToUpdate[0]==null){
+      res.status(400).send({ error:true, message: 'User not found' });
+      return;
     }
-  });
+    else if(!(await bcrypt.compare(password, userToUpdate[0].password))){
+      res.status(400).send({ error:true, message: 'Wrong Password' });
+      return;
+    }
+    else{
+      let {first_name,last_name,username,password,account_created,account_updated} = req.body;
+      if(first_name==null) first_name = userToUpdate[0].first_name;
+      if(last_name==null) last_name = userToUpdate[0].last_name;
+      if(password==null) password = userToUpdate[0].password;
+      if(username!=null || account_created!=null || account_updated!=null){
+        res.status(400).send({ error:true, message: 'Bad request' });
+        return;
+      } 
+      const hash = await bcrypt.hash(password, saltRounds);
+      await User.update(first_name,last_name,userToUpdate[0].username,hash);
+      res.status(204).send({ error:false, message: 'User successfully updated' });
+      return;
+    }
+  }
+  catch(err){
+    console.log(err);
+    res.status(400).send({ error:true, message: 'bad request' });
+  }
 };
