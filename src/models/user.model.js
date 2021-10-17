9b4e13a8e551f7f78ea2e1de3fe5f3b886256772
Pranav-Agarwal <agarwal.pran@northeastern.edu>
2021-10-17 16:48:43 -0400

    Replaced callbacks with promises
    Refactored code to use promises with async and await instead of callbacks to improve code readibility and maintainibility

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -8,38 +8,44 @@ var User = function(first_name,last_name,username,password){
     this.password = password;
 };
 
-User.create = function (first_name,last_name,username,password, result) {
-  dbConn.query("INSERT INTO user (first_name, last_name, username, password) VALUES (?,?,?,?)",[first_name,last_name,username,password], function (err, res) {
-  if(err) {
-    console.log("error: ", err);
-    result(err, null);
-  }
-  else{
-    result(null,res);
-  }
+User.create = function (first_name,last_name,username,password) {
+  return new Promise((resolve,reject) => {
+    dbConn.query("INSERT INTO user (first_name, last_name, username, password) VALUES (?,?,?,?)",[first_name,last_name,username,password], function (err, res) {
+      if(err) {
+        console.log("error: ", err);
+        reject(err);
+      }
+      else{
+        resolve(res);
+      }
+    });
   });
 };
 
-User.findUser = function (username,result) {
-  dbConn.query("Select id,first_name,last_name,username,password,account_created,account_updated from user where username = ? ", username, function (err, res) {
-  if(err) {
-    console.log("error: ", err);
-    result(err, null);
-  }
-  else{
-    result(null, res);
-  }
+User.findUser = function (username) {
+  return new Promise((resolve,reject) => {
+    dbConn.query("Select id,first_name,last_name,username,password,account_created,account_updated from user where username = ? ", username, function (err, res) {
+    if(err) {
+      console.log("error: ", err);
+      reject(err);
+    }
+    else{
+      resolve(res);
+    }
+    });
   });
 };
 
 User.update = function(first_name,last_name,username,password, result){
-  dbConn.query("UPDATE user SET first_name=?,last_name=?,password=? WHERE username = ?", [first_name,last_name,password,username], function (err, res) {
-    if(err) {
-      console.log("error: ", err);
-      result(err, null);
-    }else{
-      result(null, res);
-    }
+  return new Promise((resolve,reject) => {
+    dbConn.query("UPDATE user SET first_name=?,last_name=?,password=? WHERE username = ?", [first_name,last_name,password,username], function (err, res) {
+      if(err) {
+        console.log("error: ", err);
+        reject(err);
+      }else{
+        resolve(res);
+      }
+    });
   });
 }
 
